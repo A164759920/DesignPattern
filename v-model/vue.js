d875da3985e7e0fbcae4983400d03c01a95b0760
A164759920 <1405527125@qq.com>
2023-04-13 21:03:34 +0800

    feat:数据劫持v1.0

--- v-model/vue.js
+++ v-model/vue.js
@@ -2,7 +2,7 @@
  * TODO:
  * - 获取页面元素
  * - 放入临时内存区域
- * - 应用Vue数据 (将Vue中的数据解析到页面上)
+ * - 应用Vue数据 (将Vue中的0数据解析到页面上)
  * - 渲染页面 (减少操作DOM的次数)
  */
 class Vue {
@@ -21,21 +21,28 @@ function Observer(data_instance) {
   // 只劫持object类型
   // 递归为空或子属性非object
   if (!data_instance || typeof data_instance !== "object") return;
+  const dependency = new Dependency(data_instance);
   Object.keys(data_instance).forEach((key) => {
-    let value = data_instance[key]; // 闭包
+    let value = data_instance[key]; // 闭包 存储$data中的原始值
     Observer(value); // 递归监听对象的子属性
     Object.defineProperty(data_instance, key, {
       enumerable: true,
       configurable: true,
       get() {
-        console.log(`访问了${key}-${value}`);
+        console.log(`【getter】访问了${key}-${value}`);
+        // if (Dependency.temp) {
+        //   console.log("【getter】", Dependency.temp);
+        // }
+        // FIXME:将Watcher添加到实例dependency的list中
+        Dependency.temp && dependency.addSub(Dependency.temp);
         return value;
       },
       set(newValue) {
-        console.log(`修改了属性值:旧=${value}-新=${newValue}`);
+        console.log(`【setter】修改了属性值:旧=${value}-新=${newValue}`);
         value = newValue;
         // 若修改的newValue为对象，则也需要进行数据劫持
         Observer(newValue);
+        dependency.notify();
       },
     });
   });
@@ -57,12 +64,19 @@ function Compile(element, vm) {
     const pattern = /\{\{\s*(\S+)\s*\}\}/;
     if (node.nodeType === 3) {
       const result_regex = pattern.exec(node.nodeValue);
+      const rawNodeValue = node.nodeValue; // FIXME:保存原始的【插值表达式】
       if (result_regex) {
         const arr = result_regex[1].split(".");
         // 此处若存在对象嵌套，需要链式获取$data中的值
         const value = arr.reduce((prev, cur) => prev[cur], vm.$data);
+        console.log("frag_compile");
         // 使用replace方法进行替换 => 先匹配到{{}}中的内容,再替换为data对象中映射的内容
-        node.nodeValue = node.nodeValue.replace(pattern, value);
+        // {{name}} => 用户名 FIXME:一次替换后{{}}就被覆盖了，需要在外单独保存
+        node.nodeValue = rawNodeValue.replace(pattern, value);
+        new Watcher(vm, result_regex[1], (newValue) => {
+          console.log("节点", node);
+          node.nodeValue = rawNodeValue.replace(pattern, newValue);
+        });
       }
       return;
     }
@@ -72,4 +86,47 @@ function Compile(element, vm) {
     });
   }
   vm.$el.appendChild(fragment);
+}
+
+class Dependency {
+  constructor(flag) {
+    this.flag = flag;
+
+    this.subscribers = [];
+  }
+  // 添加的sub是每个Watcher
+  addSub(sub) {
+    this.subscribers.push(sub);
+  }
+  notify() {
+    this.subscribers.forEach((sub) => {
+      sub.update();
+    });
+  }
+}
+
+// 每一个{{}}中的值应该是一个订阅者，当发布者发布值变化时，每个订阅者自己更新{{}}
+
+// vm.$data.key => {{key}}
+class Watcher {
+  constructor(vm, key, callback) {
+    this.vm = vm;
+    // 插值表达式{{}}中的值
+    this.key = key;
+    // 回调函数即为更新{{key}}的方法
+    this.callback = callback;
+    Dependency.temp = this;
+    // 触发getter函数
+    key.split(".").reduce((prev, cur) => prev[cur], vm.$data);
+    Dependency.temp = null; // 每次添加Watcher后将该temp清空，避免重复添加
+  }
+  // update方法为实例调用，访问相应的类内属性加this
+  update() {
+    // 获取新的值
+    const newValue = this.key
+      .split(".")
+      .reduce((prev, cur) => prev[cur], this.vm.$data);
+    // 调用相应的回调函数更新页面内容
+    this.callback(newValue);
+  }
 }
